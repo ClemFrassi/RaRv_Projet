2a4f5c0475703808392d54b08518652b2e73c1f3
Dylan Lafarge <38322888+dylanLafarge@users.noreply.github.com>
2021-10-11 16:28:07 +0200

    Third Person Controller without any animation

---
+++ Assets/VR-Vs-KMS/Scripts/GameScene/ThirdPersonController/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public enum RotationAxis
+    {
+        X = 1,
+        Y = 2
+    }
+
+    public RotationAxis axes = RotationAxis.X;
+    public float SensHorizontal = 10.0f;
+    public float SensVertical = 10.0f;
+
+    public Light FlashLight;
+
+    public float MinimumVerticalAngle = -75.0f;
+    public float MaximumVerticalAngle = 55.0f;
+
+    private float rotationX = 0;
+    private float rotationY = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxis.X)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * SensHorizontal, 0, Space.Self);
+    }
+        else if (axes == RotationAxis.Y)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * SensVertical;
+            rotationX = Mathf.Clamp(rotationX, MinimumVerticalAngle, MaximumVerticalAngle);
+            rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+    }
+
+    /* if (Input.GetButtonDown("FlashLight"))
+     {
+         if (FlashLight)
+         {
+             FlashLight.enabled = !FlashLight.enabled;
+         }
+     }*/
+}
+}
+
---
+++ Assets/VR-Vs-KMS/Scripts/GameScene/ThirdPersonController/PlayerMovements.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float Speed = 5.0f;
+    public float Gravity = -14.0f;
+    public float JumpForce = 5.0f;
+
+    private CharacterController charCont;
+    private Vector3 movement;
+    private float verticalForce = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charCont = GetComponent<CharacterController>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (charCont.isGrounded)
+        {
+            verticalForce = 0;
+        }
+        else
+        {
+            verticalForce += Gravity * Time.deltaTime;
+        }
+
+        if (Input.GetButton("Jump") && charCont.isGrounded)
+        {
+            verticalForce = JumpForce;
+        }
+
+        movement = new Vector3(Input.GetAxis("Horizontal") * Speed, verticalForce, Input.GetAxis("Vertical") * Speed);
+        movement = transform.TransformDirection(movement);
+        charCont.Move(movement * Time.deltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        body.AddForceAtPosition(charCont.velocity * 0.1f, hit.point, ForceMode.Impulse);
+    }
+}
