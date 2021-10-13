b9e66ba0960348b60abb084e010d5beddc1232fd
Dylan Lafarge <38322888+dylanLafarge@users.noreply.github.com>
2021-10-13 14:06:25 +0200

    Add shoot to the TPS, and some movement animation

---
+++ Assets/VR-Vs-KMS/Scripts/GameScene/TPSShootController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSShootController : MonoBehaviour
+{
+    public GameObject ChargePrefab;
+    public int force = 50;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.yellow, 1.5f);
+            RaycastHit hit;
+
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
+            {
+                Debug.Log(hit.collider.name);
+                GameObject Charge = Instantiate(ChargePrefab, gameObject.transform.position, gameObject.transform.rotation);
+                Charge.GetComponent<ChargeController>().SetTag("Antiviral");
+                Charge.GetComponent<Rigidbody>().velocity = (hit.point - transform.position) * force;
+
+                //Rigidbody ballRb = hit.rigidbody;
+                //ballRb.AddForce((hit.transform.localPosition - hit.point) * force);
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        GameObject Charge = Instantiate(ChargePrefab, gameObject.transform.position, gameObject.transform.rotation);
+        Charge.GetComponent<ChargeController>().SetTag("Antiviral");
+        Charge.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * force);
+    }
+}
