8097bd46ff448c9870a677566414c41f25e910e2
ClemFrassi <clementfrassinelli@gmail.com>
2021-10-20 16:07:55 +0200

    Debug

--- Assets/VR-Vs-KMS/Scripts/ThrowableObject.cs
+++ Assets/VR-Vs-KMS/Scripts/ThrowableObject.cs
@@ -57,7 +57,6 @@ public class ThrowableObject : MonoBehaviourPunCallbacks
         if (other.gameObject.GetComponentInChildren<PlayerBehaviour>())
         {
             photonView.RPC("Explosion", RpcTarget.AllViaServer, other.GetComponent<PlayerBehaviour>().photonView.ViewID);
-            explosive = false;
         }
         StartCoroutine(Delete());    
 
